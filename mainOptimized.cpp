be7c87b2a91f420972dd699865b7f30f8176ca46
ActuallyMadhav <madhavra.2004@gmail.com>
2026-02-25 00:31:06 +0800

    implemented all functions except shader

--- mainOptimized.cpp
+++ mainOptimized.cpp
@@ -8,17 +8,58 @@
 
 // brightness scale (lowest to highest): .,-~:;=!*#$@
 
-std::array<float,3> coords3d(float theta, float phi, float R, float r);
+std::array<float,3> coords3d(float theta, float phi, float R, float r){
+    float x = (R + r*cos(theta)) * cos(phi);
+    float y = (R + r*cos(theta)) * sin(phi);
+    float z = r*sin(theta);
 
-std::array<float,3> rotateX(std::array<float,3>& coords, float A);
+    return {x, y, z};
+}
 
-std::array<float,3> rotateZ(std::array<float,3>& coords, float B);
+std::array<float,3> rotateX(std::array<float,3>& coords, float A){
+    float y_transformed = coords[1]*cos(A) - coords[2]*sin(A);
+    float z_transformed = coords[1]*sin(A) + coords[2]*cos(A);
+    float x_transformed = coords[0]; // x remains unchanged
 
-std::array<float,2> projection(std::array<float,3>& coords);
+    return {x_transformed, y_transformed, z_transformed};
+}
 
-std::array<float,2> scaleAndShift(std::array<float,2>& coords);
+std::array<float,3> rotateZ(std::array<float,3>& coords, float B){
+    float x_transformed = coords[0]*cos(B) - coords[1]*sin(B);
+    float y_transformed = coords[0]*sin(B) + coords[1]*cos(B);
+    float z_transformed = coords[2]; // z remains unchanged
 
-void normalize(std::array<float,3>& v);
+    return {x_transformed, y_transformed, z_transformed};
+}
+
+std::array<float,2> projection(std::array<float,3>& coords){
+    int K = 5;
+
+    float X = coords[0] / (coords[2] + K);
+    float Y = coords[1] / (coords[2] + K);
+
+    return {X, Y};
+}
+
+std::array<float,2> scaleAndShift(std::array<float,2>& coords){
+    float X_scaled = coords[0] * 30;
+    float Y_scaled = coords[1] * 15;
+
+    float X_final = X_scaled + 40;
+    float Y_final = Y_scaled + 12;
+
+    return {X_final, Y_final};
+}
+
+void normalize(std::array<float,3>& v){
+    float magnitude = sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+
+    if(!magnitude) return;
+
+    v[0] /= magnitude;
+    v[1] /= magnitude;
+    v[2] /= magnitude;
+}
 
 char shader(float theta, float phi);
 
