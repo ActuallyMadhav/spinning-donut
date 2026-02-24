750abd7f23a7b468f3c1a726e10c1a9a99f0921f
ActuallyMadhav <madhavra.2004@gmail.com>
2026-02-25 00:50:31 +0800

    had to create my own vec2 and vec3 to return coords

--- mainInC.c
+++ mainInC.c
@@ -4,4 +4,22 @@
 
 #define pi 3.141582654
 
+typedef struct{
+    float x, y
+} Vec2;
+
+typedef struct{
+    float x,y,z
+} Vec3;
+
 // brightness scale (lowest to highest): .,-~:;=!*#$@
+
+Vec3 coords3D(float theta, float phi, float R, float r){
+    Vec3 coords;
+
+    coords.x = (R + r*cos(theta)) * cos(phi);
+    coords.y = (R + r*cos(theta)) * sin(phi);
+    coords.z = r*sin(theta);
+
+    return coords;
+}
