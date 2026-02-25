619db475270a8d96cfd8f99948a3191a502dc8fd
ActuallyMadhav <madhavra.2004@gmail.com>
2026-02-25 19:26:57 +0800

    changed pi to M_PI from cmath

--- mainInC.c
+++ mainInC.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 #include <unistd.h>
-
-#define pi 3.141582654
-
-typedef struct{
-    float x, y
-} Vec2;
-
-typedef struct{
-    float x,y,z
-} Vec3;
+#include "vec.h"
 
 // brightness scale (lowest to highest): .,-~:;=!*#$@
 
-Vec3 coords3D(float theta, float phi, float R, float r){
-    Vec3 coords;
+vec3 coords3D(float theta, float phi, float R, float r){
+    vec3 coords;
 
     coords.x = (R + r*cos(theta)) * cos(phi);
     coords.y = (R + r*cos(theta)) * sin(phi);
     coords.z = r*sin(theta);
 
     return coords;
+}
+
+int main(){
+
+    vec3 coords = coords3D(0.0, 0.0, 2.0, 1.0);
+
+    printf("%f%f%f", coords.x, coords.y, coords.z);
+
+    return 0;
 }
