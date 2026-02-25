19615b4f21d3e6afcca81620d0c9192e7205a194
ActuallyMadhav <madhavra.2004@gmail.com>
2026-02-25 19:27:25 +0800

    making my own math library

---
+++ vec.h
@@ -0,0 +1,20 @@
+#ifndef VEC_H
+#define VEC_H
+
+#include <stdio.h>
+#include <math.h>
+
+typedef struct{
+    float x, y;
+} vec2;
+
+typedef struct{
+    float x, y, z;
+} vec3;
+
+typedef struct{
+    float x, y, z, w;
+} vec4;
+
+
+#endif
